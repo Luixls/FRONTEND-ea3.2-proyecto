75996f01c996261d25afa8bcfcb1b44421b721d2
Luixls <luisaraujo1998@gmail.com>
2024-06-15 17:17:25 -0400

    Imágenes para "Equipo" e importarlas
    Importar las imágenes dentro de index.js para que sean compiladas por Webpack
    
    Co-Authored-By: FN2184 <159496056+fn2184@users.noreply.github.com>
    Co-Authored-By: jonathanuvm <127060318+jonavnet@users.noreply.github.com>

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,8 +1,62 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && (!scriptUrl || !/^http(s?):/.test(scriptUrl))) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/imgs/equipo1.jpg
+const equipo1_namespaceObject = __webpack_require__.p + "imgs/equipo1.jpg";
+;// CONCATENATED MODULE: ./src/imgs/equipo2.jpg
+const equipo2_namespaceObject = __webpack_require__.p + "imgs/equipo2.jpg";
+;// CONCATENATED MODULE: ./src/imgs/equipo3.jpg
+const equipo3_namespaceObject = __webpack_require__.p + "imgs/equipo3.jpg";
+;// CONCATENATED MODULE: ./src/index.js
 
 
 console.log("Archivo index.js en ejecución...")
+
+// Importar las imágenes para que Webpack las procese
+;
+
+
+
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./estilos.css";
 
 console.log("Archivo index.js en ejecución...")
+
+// Importar las imágenes para que Webpack las procese
+import "./imgs/equipo1.jpg";
+import "./imgs/equipo2.jpg";
+import "./imgs/equipo3.jpg";
